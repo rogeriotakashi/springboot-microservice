56e50da2e8f9245a60943062328b119315955e2d
rogeriotakashi <rogeriotakashi@hotmail.com>
2019-12-21 17:01:44 -0300

    Add Hystrix command properties

--- movie-catalog-service/src/main/java/io/rogeriotakashi/moviecatalogservice/services/MovieInfo.java
+++ movie-catalog-service/src/main/java/io/rogeriotakashi/moviecatalogservice/services/MovieInfo.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 
 import io.rogeriotakashi.moviecatalogservice.domain.CatalogItem;
 import io.rogeriotakashi.moviecatalogservice.domain.Movie;
@@ -17,7 +18,13 @@ public class MovieInfo {
 	private RestTemplate restTemplate;
 	
 	
-	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem")
+	@HystrixCommand(fallbackMethod = "getFallbackCatalogItem",
+					commandProperties= {
+							   @HystrixProperty(name="execution.isolation.thread.timeoutInMilliseconds",value="2000"),
+							   @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value="5"),
+							   @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value="50"),
+							   @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value="5000")})
+	
 	public CatalogItem getCatalogItem(Rating rating) {
 		Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class);
 		return new CatalogItem(movie.getName(),movie.getOverview(),rating.getRating());
