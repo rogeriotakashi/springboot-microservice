225ed591eca2211dad6913b13c0107c1ba74501e
rogeriotakashi <rogeriotakashi@hotmail.com>
2019-12-23 16:54:46 -0200

    Add: First Spring Feign Implementantion (Calling UsuarioResource from projetxbackend project)

--- movie-main-service/src/main/java/io/rogeriotakashi/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-main-service/src/main/java/io/rogeriotakashi/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
@@ -13,6 +14,7 @@ import org.springframework.web.reactive.function.client.WebClient;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableCircuitBreaker
+@EnableFeignClients
 public class MovieCatalogServiceApplication {
 
 	@Bean
