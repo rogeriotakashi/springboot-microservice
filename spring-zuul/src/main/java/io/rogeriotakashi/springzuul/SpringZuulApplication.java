0bea5f65c1df4a5b0c9f86d16521f78b9d053ab1
rogeriotakashi <rogeriotakashi@hotmail.com>
2020-01-02 22:25:31 -0300

    Edited Add Zuul Proxy

---
+++ spring-zuul/src/main/java/io/rogeriotakashi/springzuul/SpringZuulApplication.java
@@ -0,0 +1,13 @@
+package io.rogeriotakashi.springzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringZuulApplication.class, args);
+	}
+
+}
---
+++ spring-zuul/src/main/java/io/rogeriotakashi/springzuul/config/ZuulConfig.java
@@ -0,0 +1,10 @@
+package io.rogeriotakashi.springzuul.config;
+
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableZuulProxy
+public class ZuulConfig {
+
+}
